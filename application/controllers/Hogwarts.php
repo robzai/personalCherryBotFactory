75da47bc05124ac5769f0b47e6d181544bfbe73c
s-rebecca-fu <rebeccafu1991@gmail.com>
2017-02-08 13:53:33 -0500

    first commit

---
+++ application/controllers/Hogwarts.php
@@ -0,0 +1,46 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Hogwarts extends Application
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Homepage for our app
+	 */
+	public function index()
+	{
+		// this is the view we want shown
+		$this->data['pagebody'] = 'homepage';
+
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();
+		$authors = array ();
+		foreach ($source as $record)
+		{
+			$authors[] = array ('who' => $record['who'],
+                                            'mug' => $record['mug'],
+                                            'href' => $record['where']                                            
+                                            );
+		}
+		$this->data['authors'] = $authors;
+
+		$this->render();
+	}
+        
+        public function shucks(){
+            
+            // this is the view we want shown
+            $this->data['pagebody'] = 'justone';
+            
+            $this->data = array_merge($this->data, $this->quotes->get(2));
+
+            $this->render();
+        }
+
+}
