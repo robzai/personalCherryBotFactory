4b6ba046924d1e55bac17e7df377a7c2d33fc644
robzai <robzai@hotmail.com>
2017-03-29 14:54:07 -0700

    Update BotFactory.sql

--- data/BotFactory.sql
+++ data/BotFactory.sql
@@ -23,25 +23,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `robot`
+-- Table structure for table `robots`
 --
 
-DROP TABLE IF EXISTS `robot`;
-CREATE TABLE `robot` (
+DROP TABLE IF EXISTS `robots`;
+CREATE TABLE `robots` (
   `id` int(8) NOT NULL,
   `top` varchar(10) DEFAULT NULL,
   `torso` varchar(10) DEFAULT NULL,
   `bottom` varchar(10) DEFAULT NULL,
   --`pic` varchar(50) DEFAULT NULL,
   `date` date DEFAULT NULL,
-  `unitprice` int(6) DEFAULT NULL
+  `price` int(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `robot`
 --
 
-INSERT INTO `robot` ( `id`,`top`,`torso`,`bottom`,`date`,`unitprice`）
+INSERT INTO `robots` ( `id`,`top`,`torso`,`bottom`,`date`,`price`）
   VALUES
   (1, '12234A1', '90062R2', '3276C3', '2012-1-1', 1),
   (2, '00002A1', '7890B2', 'ABCDC3', '2012-1-1', 1),
@@ -69,18 +69,18 @@ CREATE TABLE 'parts' (
 --
 
 INSERT INTO 'parts' ('id', 'ca', 'pic', 'plant', 'date', 'unitprice', 'type') VALUES
-(1, '12234A1', 'a1', 'Apple', 2010-01-01, 9, 'top'),
+(1, '12234A1', 'a1', 'Apple', 2010-01-01, 10, 'top'),
 (2, '00002A1', 'a1', 'Durian', 2016-01-12, 10, 'top'),
 (3, '06BFB1', 'b1', 'Apple', 2017-01-01, 10, 'top'),
-(4, '7890B2', 'b2', 'Apple', 2016-01-12, 7, 'torso'),
-(5, 'AABB0C2', 'c2', 'Banana', 2016-03-12, 5, 'torso'),
-(6, '3276C3', 'c3', 'Durian', 2016-01-12, 8, 'bottom'),
-(7, 'ABCDC3', 'c3', 'Red Umbrella', 2016-01-12, 8, 'bottom'),
-(8, '0011R1', 'r1', 'Banana', 2016-04-05, 9, 'top'),
+(4, '7890B2', 'b2', 'Apple', 2016-01-12, 10, 'torso'),
+(5, 'AABB0C2', 'c2', 'Banana', 2016-03-12, 10, 'torso'),
+(6, '3276C3', 'c3', 'Durian', 2016-01-12, 10, 'bottom'),
+(7, 'ABCDC3', 'c3', 'Red Umbrella', 2016-01-12, 10, 'bottom'),
+(8, '0011R1', 'r1', 'Banana', 2016-04-05, 10, 'top'),
 (9, '90062R2', 'r2', 'Running man', 2016-01-12, 10, 'torso'),
-(10, 'ADC23R3', 'r3', 'Red Umbrella', 2016-01-12, 20, 'bottom'),
-(11, '90786W2', 'w2', 'Durian', 2017-01-12, 7, 'torso'),
-(12, 'AD454W3', 'w3', 'Banana', 2017-01-12, 12, 'bottom');
+(10, 'ADC23R3', 'r3', 'Red Umbrella', 2016-01-12, 10, 'bottom'),
+(11, '90786W2', 'w2', 'Durian', 2017-01-12, 10, 'torso'),
+(12, 'AD454W3', 'w3', 'Banana', 2017-01-12, 10, 'bottom');
 
 -- --------------------------------------------------------
 
@@ -131,87 +131,6 @@ INSERT INTO 'histories' ('type', 'partstype', 'date', 'price')
   ('Sold Robot', 'w1 w2 w3', '04:30am April 24 2014', 200),
   ('Sold Robot', 'w1 y2 z3', '01:30am April 25 2014', 100);
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sizes`
---
-
-DROP TABLE IF EXISTS `sizes`;
-CREATE TABLE `sizes` (
-  `id` int(1) NOT NULL,
-  `name` varchar(6) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `sizes`
---
-
-INSERT INTO `sizes` (`id`, `name`) VALUES
-(1, 'small'),
-(2, 'medium'),
-(3, 'large');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `statuses`
---
-
-DROP TABLE IF EXISTS `statuses`;
-CREATE TABLE `statuses` (
-  `id` int(1) NOT NULL,
-  `name` varchar(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `statuses`
---
-
-INSERT INTO `statuses` (`id`, `name`) VALUES
-(1, 'in progress'),
-(2, 'complete');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tasks`
---
-
-DROP TABLE IF EXISTS `tasks`;
-CREATE TABLE `tasks` (
-  `id` int(2) NOT NULL,
-  `task` varchar(22) DEFAULT NULL,
-  `priority` int(1) DEFAULT NULL,
-  `size` int(1) DEFAULT NULL,
-  `group` int(1) DEFAULT NULL,
-  `deadline` varchar(8) DEFAULT NULL,
-  `status` varchar(1) DEFAULT NULL,
-  `flag` varchar(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `tasks`
---
-
-INSERT INTO `tasks` (`id`, `task`, `priority`, `size`, `group`, `deadline`, `status`, `flag`) VALUES
-(1, 'COMP1234 assignment', 3, 2, 2, '20170219', '1', ''),
-(2, 'Mow the lawn', 2, 2, 1, '', '2', ''),
-(3, 'Wash the car', 2, 2, 1, '', '', ''),
-(4, 'Paint the fence', 1, 2, 1, '', '', ''),
-(5, 'Study for midterms', 3, 3, 2, '', '', ''),
-(6, 'Intramural hockey game', 1, 2, 4, '', '', ''),
-(7, 'Canucks hockey game', 3, 3, 4, '20170305', '', ''),
-(8, 'Buy steel-toed boots', 2, 1, 3, '', '', ''),
-(9, 'Learn French', 1, 3, 3, '20161231', '1', ''),
-(10, 'Hit the gym', 2, 1, 4, '', '', ''),
-(11, 'Pay bills', 3, 1, 1, '', '', ''),
-(12, 'Meet George', 2, 1, 1, '', '', ''),
-(13, 'Buy milk & bread', 2, 1, 1, '', '', '1'),
-(14, 'Read War & Peace', 1, 3, 1, '', '', ''),
-(15, 'Organize the study', 1, 4, 1, '', '', '');
-
 --
 -- Indexes for dumped tables
 --
@@ -221,48 +140,12 @@ ALTER TABLE `parts`
 --
 -- Indexes for table `robot`
 --
-ALTER TABLE `robot`
+ALTER TABLE `robots`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `groups`
+-- Indexes for table `histories`
 --
-ALTER TABLE `groups`
+ALTER TABLE `histories`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `priorities`
---
-ALTER TABLE `priorities`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `sizes`
---
-ALTER TABLE `sizes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `statuses`
---
-ALTER TABLE `statuses`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tasks`
---
-ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tasks`
---
-ALTER TABLE `tasks`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
