560610f451b7e0669544538ab6fdd4e5cf9b0aa6
s-rebecca-fu <rebeccafu1991@gmail.com>
2017-03-29 19:19:22 -0400

    role added

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('parser', 'database', 'parsedown','session');
+$autoload['libraries'] = array('parser', 'database','session');
 
 
 /*
